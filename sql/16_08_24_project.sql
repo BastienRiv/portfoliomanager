0ed3ecd9a74a63ee74707470c61172efb94fad73
Sebastian Hernandez <octavio.hpr@gmail.com>
2024-08-16 22:31:33 +0000

    sql script fixed

--- sql/16_08_24_project.sql
+++ sql/16_08_24_project.sql
@@ -9,5 +9,9 @@ CREATE TABLE user (
     country varchar(20) not null
 );
 
-INSERT INTO user (fname, birthday, country) VALUES ('Mariano', '15-04-1994', 'Mexico');
-INSERT INTO user (fname, birthday, country) VALUES ('Sebas', '23-08-1996', 'Mexico');
+--INSERT INTO user (fname, birthday, country) VALUES ('Mariano', '15-04-1994', 'Mexico');
+--INSERT INTO user (fname, birthday, country) VALUES ('Sebas', '23-08-1996', 'Mexico');
+
+--En mi computadora funciono con el siguiente formato atte Sebas:
+INSERT INTO user (fname, birthday, country) VALUES ('Mariano', '1994-04-15', 'Mexico');
+INSERT INTO user (fname, birthday, country) VALUES ('Sebas', '1995-08-23', 'Mexico');
