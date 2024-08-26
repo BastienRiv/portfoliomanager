97c0bf637697857b92ef0d0dc5494b7e01c7a157
Sebastian Hernandez <octavio.hpr@gmail.com>
2024-08-26 05:38:35 +0000

    refactored code, added beta version of the dashboard

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 "use strict"
 
-//const express = require('express');
-//const {executeQuery} = require('./database-setup.js')
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-
 import express from 'express'
 import { executeQuery } from './database-setup.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
 
 // Express App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const __filename = fileURLToPath(import.meta.url);
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// --- Middleware
-
+// Middleware
 app.use((req, res, next) => {
   console.log('--- Incoming Request --- ' + "Time: " + Date.now());
   console.log('Method: ' + req.method + ' | URL: ' + req.originalUrl);
@@ -30,55 +24,96 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.use(express.static(path.join(__filename, 'public')));
+
+
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__filename, '../public/index.html'))
+})
+
+app.get('/api', (req, res) => {
   res.send('This API is live!')
 })
 
+
 app.get('/user', (req, res) => {
   const query = `SELECT * from user;`;
-
   executeQuery(query,[],res);
 });
 
-app.get('/company', (req, res) => {
-  const query = `SELECT * from company;`;
 
-  executeQuery(query, [] , res);
-})
+app.get('/dashboard', async (req,res) => {
+  try {
+    const { city, state } = req.query;
+    let query = `
+      SELECT emp.emp_id, job.job_desc
+      FROM pubs.employee AS emp
+      JOIN pubs.jobs AS job ON emp.job_id = job.job_id
+    `;
 
-app.get('/stocks', (req, res) => {
-  const query = `SELECT * from stocks;`;
+    const params = [];
+    if (city && state) {
+      query += `WHERE emp.city = ? AND emp.state = ?`;
+      params.push(city, state);
+    }
 
-  executeQuery(query, [] , res);
-})
+    const results = await executeQuery(query, params, res);
 
-app.get('/buy', (req, res) => {
-  const query = `SELECT * from buy;`;
+    const jobCounts = {};
+    results.forEach((employee) => {
+      const jobDesc = employee.job_desc;
+      jobCounts[jobDesc] = (jobCounts[jobDesc] || 0) + 1;
+    });
 
-  executeQuery(query, [] , res);
-})
+    res.json({ jobCounts });
+  } catch (error) {
+    console.error('Database query error:', error);
+    res.status(500).json({ error: 'Database query error' });
+  }
+});
 
-app.get('/sell', (req, res) => {
-  const query = `SELECT * from sell;`;
+app.get('/profit', (req, res) => {
+  const query = `
+                WITH bu AS (
+                SELECT id_company, SUM(tot_investment) AS tot_investment
+                FROM portfolio.buy
+                GROUP BY id_company
+            ),
+            sel AS (
+                SELECT id_company, SUM(tot_sold) AS tot_sold
+                FROM portfolio.sell
+                GROUP BY id_company
+            ),
+            principal AS (
+            SELECT
+                COALESCE(bu.id_company, sel.id_company) AS id_company,
+                COALESCE(bu.tot_investment, 0) AS tot_investment,
+                COALESCE(sel.tot_sold, 0) AS tot_sold
+            FROM
+                bu
+            LEFT JOIN
+                sel ON bu.id_company = sel.id_company
+            UNION ALL
+            SELECT
+                COALESCE(bu.id_company, sel.id_company) AS id_company,
+                COALESCE(bu.tot_investment, 0) AS tot_investment,
+                COALESCE(sel.tot_sold, 0) AS tot_sold
+            FROM
+                sel
+            LEFT JOIN
+                bu ON sel.id_company = bu.id_company)
+            Select id_company, (tot_investment - tot_sold) as PL FROM principal   
+  `;
+  executeQuery(query,[],res);
+});
 
-  executeQuery(query, [] , res);
-})
-
+// Catch-all route for any other requests
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not found' });
+});
 
 const port = 4000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
-});
-
-
-const app1 = express();
-
-app1.use(express.static(path.join(__filename, 'public')));
-
-app1.get('/', (req, res) => {
-  res.sendFile(path.join(__filename, '../public/index.html'))
-} )
-
-app1.listen(3007, () => {
-    console.log(`Server is running on http://localhost:${3007}`);
 });
