1dbf331c2e1c794b6b568e0896ea08a0236f9314
BastienRiv <octavio.hpr@gmail.com>
2024-08-29 14:43:11 -0600

    portfolio page added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import { executeQuery } from './database-setup.js';
-import performanceRouter from './routes/performance.js';
+import { executeQuery } from "./database-setup.js";
+import performanceRouter from "./routes/performance.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,106 +18,155 @@ app.use(cors());
 
 // Middleware
 app.use((req, res, next) => {
-    console.info('--- Incoming Request --- ' + 'Time: ' + Date.now());
-    console.info('Method: ' + req.method + ' | URL: ' + req.originalUrl);
-    console.info('Headers:', JSON.stringify(req.headers, null, 2));
-    console.info('Body:', JSON.stringify(req.body, null, 2));
-    console.info('Query Params:', JSON.stringify(req.query, null, 2));
-    console.info('------------------------');
-    next();
+  console.info("--- Incoming Request --- " + "Time: " + Date.now());
+  console.info("Method: " + req.method + " | URL: " + req.originalUrl);
+  console.info("Headers:", JSON.stringify(req.headers, null, 2));
+  console.info("Body:", JSON.stringify(req.body, null, 2));
+  console.info("Query Params:", JSON.stringify(req.query, null, 2));
+  console.info("------------------------");
+  next();
 });
 
-app.use('/api/performance', performanceRouter);
+app.use("/api/performance", performanceRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/portfolio', (req, res) => {
-    res.sendFile(path.join(__filename, '../public/portfolio.html'));
+app.get("/portfolio", (req, res) => {
+  res.sendFile(path.join(__filename, "../public/portfolio.html"));
 });
 
-app.get('/purchase', (req, res) => {
-    res.sendFile(path.join(__filename, '../public/pages/purchase/purchase.html'));
+app.get("/purchase", (req, res) => {
+  res.sendFile(path.join(__filename, "../public/pages/purchase/purchase.html"));
 });
 
-app.get('/sale', (req, res) => {
-    res.sendFile(path.join(__filename, '../public/sale.html'));
+app.get("/sale", (req, res) => {
+  res.sendFile(path.join(__filename, "../public/sale.html"));
 });
 
-app.get('/api', (req, res) => {
-    res.send('This API is live!');
+app.get("/api", (req, res) => {
+  res.send("This API is live!");
 });
 
-app.get('/api/user', (req, res) => {
-    const query = `SELECT * from user;`;
-    executeQuery(query, [], res)
-        .then((result) => {
-            res.status(200).json(result);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed' });
-        });
+app.get("/api/user", (req, res) => {
+  const query = `SELECT * from user;`;
+  executeQuery(query, [], res)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed" });
+    });
 });
 
-app.get('/api/companies', (req, res) => {
-    const query = `SELECT id_company, cname from company;`;
-    executeQuery(query, [], res)
-        .then((result) => {
-            res.status(200).json(result);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed' });
-        });
+app.get("/api/companies", (req, res) => {
+  const query = `SELECT id_company, cname from company;`;
+  executeQuery(query, [], res)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed" });
+    });
 });
 
-app.get('/api/stockprice', (req, res) => {
-    const { id_company, sdate } = req.query;
-
-    const query = `select adj_close from stocks where id_company= ? and sdate = ?;`;
-
-    executeQuery(query, [id_company, sdate], res)
-        .then((result) => {
-            res.status(200).json(result);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed' });
-        });
+app.get("/api/currentStocks", (req, res) => {
+  const query = `WITH StocksBought AS (
+                SELECT
+                    id_company,
+                    SUM(stocks_bought) AS total_bought,
+                    SUM(cost) AS total_investment
+                FROM buy
+                WHERE id = 1
+                GROUP BY id_company
+            ),
+            StocksSold AS (
+                SELECT
+                    id_company,
+                    SUM(stocks_sold) AS total_sold
+                FROM sell
+                WHERE id = 1
+                GROUP BY id_company
+            ),
+            StocksCurrent AS (
+                SELECT
+                    b.id_company,
+                    COALESCE(b.total_bought, 0) - COALESCE(s.total_sold, 0) AS current_stock
+                FROM StocksBought b
+                LEFT JOIN StocksSold s ON b.id_company = s.id_company
+                UNION
+                SELECT
+                    s.id_company,
+                    COALESCE(b.total_bought, 0) - COALESCE(s.total_sold, 0) AS current_stock
+                FROM StocksSold s
+                LEFT JOIN StocksBought b ON b.id_company = s.id_company
+            )
+            SELECT
+                c.id_company AS Company,
+                c.current_stock AS Quantity,
+                b.total_investment AS Total_Cost
+            FROM StocksCurrent c
+            LEFT JOIN StocksBought b ON c.id_company = b.id_company
+            WHERE c.current_stock > 0
+            ORDER BY c.id_company;
+            ;`;
+  executeQuery(query, [], res)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed" });
+    });
 });
 
-app.get('/api/dashboard', async (req, res) => {
-    try {
-        const { city, state } = req.query;
-        let query = `
+app.get("/api/stockprice", (req, res) => {
+  const { id_company, sdate } = req.query;
+
+  const query = `select adj_close from stocks where id_company= ? and sdate = ?;`;
+
+  executeQuery(query, [id_company, sdate], res)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed" });
+    });
+});
+
+app.get("/api/dashboard", async (req, res) => {
+  try {
+    const { city, state } = req.query;
+    let query = `
       SELECT emp.emp_id, job.job_desc
       FROM pubs.employee AS emp
       JOIN pubs.jobs AS job ON emp.job_id = job.job_id
     `;
 
-        const params = [];
-        if (city && state) {
-            query += `WHERE emp.city = ? AND emp.state = ?`;
-            params.push(city, state);
-        }
-
-        const results = await executeQuery(query, params, res);
-
-        const jobCounts = {};
-        results.forEach((employee) => {
-            const jobDesc = employee.job_desc;
-            jobCounts[jobDesc] = (jobCounts[jobDesc] || 0) + 1;
-        });
-
-        res.json({ jobCounts });
-    } catch (error) {
-        res.status(500).json({ error: 'Database query error' });
+    const params = [];
+    if (city && state) {
+      query += `WHERE emp.city = ? AND emp.state = ?`;
+      params.push(city, state);
     }
+
+    const results = await executeQuery(query, params, res);
+
+    const jobCounts = {};
+    results.forEach((employee) => {
+      const jobDesc = employee.job_desc;
+      jobCounts[jobDesc] = (jobCounts[jobDesc] || 0) + 1;
+    });
+
+    res.json({ jobCounts });
+  } catch (error) {
+    res.status(500).json({ error: "Database query error" });
+  }
 });
 
-app.get('/api/profit', (req, res) => {
-    const query = `
+app.get("/api/profit", (req, res) => {
+  const query = `
                 WITH bu AS (
                 SELECT id_company, SUM(tot_investment) AS tot_investment
                 FROM portfolio.buy
@@ -149,70 +198,130 @@ app.get('/api/profit', (req, res) => {
             Select id_company, (tot_investment - tot_sold) as PL FROM principal   
   `;
 
-    executeQuery(query, [], res)
-        .then((result) => {
-            res.status(200).json(result);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed to add Purchase' });
-        });
+  executeQuery(query, [], res)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to add Purchase" });
+    });
 });
 
 // Gets ALL buy transactions from company
-app.get('/api/transactions/:type/:id_company', (req, res) => {
-    const idCompany = req.params.id_company;
-    const type = req.params.type; // type = 'sell' || 'buy'
+app.get("/api/transactions/:type/:id_company", (req, res) => {
+  const idCompany = req.params.id_company;
+  const type = req.params.type; // type = 'sell' || 'buy'
 
-    if (!idCompany) {
-        return res.status(400).json({ error: 'id_company route parameter is required: .../:id_company' });
-    }
+  if (!idCompany) {
+    return res.status(400).json({
+      error: "id_company route parameter is required: .../:id_company",
+    });
+  }
 
-    const query = `SELECT * FROM ${type} WHERE id_company = ?`;
-    executeQuery(query, [idCompany], res)
-        .then((results) => {
-            res.status(200).json(results);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed to retrieve purchases' });
-        });
+  const query = `SELECT * FROM ${type} WHERE id_company = ?`;
+  executeQuery(query, [idCompany], res)
+    .then((results) => {
+      res.status(200).json(results);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to retrieve purchases" });
+    });
 });
 
-app.post('/api/purchase', function (req, res) {
-    const { id_transactionb, bdate, stocks_bought, cost, tot_investment, id, id_company, id_stock } = req.body;
+// Gets all buy/sell transactions
+app.get("/api/transaction/:type", (req, res) => {
+  const type = req.params.type; // type = 'sell' || 'buy'
 
-    const query = `INSERT INTO buy (id_transactionb, bdate, stocks_bought, cost, tot_investment, id, id_company, id_stock) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?);`;
-
-    executeQuery(query, [id_transactionb, bdate, stocks_bought, cost, tot_investment, id, id_company, id_stock], res)
-        .then(() => {
-            res.status(200).json({ message: 'Purchase added successfully' });
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed to add Purchase' });
-        });
+  const query = `SELECT * FROM ${type}`;
+  executeQuery(query, [], res)
+    .then((results) => {
+      res.status(200).json(results);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to retrieve " });
+    });
 });
 
-app.post('/api/sale', function (req, res) {
-    const { id_transactions, sdate, stocks_sold, adj_cost, tot_sold, id, id_company, id_stock } = req.body;
+app.post("/api/purchase", function (req, res) {
+  const {
+    id_transactionb,
+    bdate,
+    stocks_bought,
+    cost,
+    tot_investment,
+    id,
+    id_company,
+    id_stock,
+  } = req.body;
 
-    const query = `INSERT INTO sell (id_transactions, sdate, stocks_sold, adj_cost, tot_sold, id, id_company, id_stock) 
+  const query = `INSERT INTO buy (id_transactionb, bdate, stocks_bought, cost, tot_investment, id, id_company, id_stock) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?);`;
 
-    executeQuery(query, [id_transactions, sdate, stocks_sold, adj_cost, tot_sold, id, id_company, id_stock], res)
-        .then(() => {
-            res.status(200).json({ message: 'Sale added successfully' });
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed to add Sale' });
-        });
+  executeQuery(
+    query,
+    [
+      id_transactionb,
+      bdate,
+      stocks_bought,
+      cost,
+      tot_investment,
+      id,
+      id_company,
+      id_stock,
+    ],
+    res
+  )
+    .then(() => {
+      res.status(200).json({ message: "Purchase added successfully" });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to add Purchase" });
+    });
+});
+
+app.post("/api/sale", function (req, res) {
+  const {
+    id_transactions,
+    sdate,
+    stocks_sold,
+    adj_cost,
+    tot_sold,
+    id,
+    id_company,
+    id_stock,
+  } = req.body;
+
+  const query = `INSERT INTO sell (id_transactions, sdate, stocks_sold, adj_cost, tot_sold, id, id_company, id_stock) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?);`;
+
+  executeQuery(
+    query,
+    [
+      id_transactions,
+      sdate,
+      stocks_sold,
+      adj_cost,
+      tot_sold,
+      id,
+      id_company,
+      id_stock,
+    ],
+    res
+  )
+    .then(() => {
+      res.status(200).json({ message: "Sale added successfully" });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to add Sale" });
+    });
 });
 
 // Catch-all route for any other requests
 app.use((req, res) => {
-    res.status(404).json({ error: 'Not found' });
+  res.status(404).json({ error: "Not found" });
 });
 
 const port = 4001;
 app.listen(port, () => {
-    console.info(`Server running at http://localhost:${port}/`);
+  console.info(`Server running at http://localhost:${port}/`);
 });
